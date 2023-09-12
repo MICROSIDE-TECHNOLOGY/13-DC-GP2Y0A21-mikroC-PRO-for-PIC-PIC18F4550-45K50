0d4eaa5adb126aec28390d02da8b53022d12ab3a
vhperaltaj <victor.peralta@microside.com>
2023-09-12 12:45:32 -0600

    Updated indentation

--- Proyecto/4550/MotorDCInfrarrojo.c
+++ Proyecto/4550/MotorDCInfrarrojo.c
@@ -18,19 +18,18 @@ Para usar el código con bootloader, configurar como lo indica MICROSIDE:
 
 #pragma orgall 0x1FFF
 
-int VALOR_ADC;                                  //Variable para almacenar el valor leído del ADC
+int VALOR_ADC;                                // Variable para almacenar el valor leído del ADC
 
-void main(void) org 0x2000
+void main( void ) org 0x2000
 {
-     TRISC = 0X00;                              // Declaramos el puerto como salida
-     PWM1_Init (5000);                          // Inicializa PWM a 5000 Hz
-     PWM1_Start ();
-     TRISA0_bit = 1;                            //PIN como entrada
-     ADC_Init_Advanced(_ADC_INTERNAL_REF);      //Voltaje de referencia a VDD y GND
+   TRISC = 0X00;                              // Declaramos el puerto como salida
+   PWM1_Init( 5000 );                         // Inicializa PWM a 5000 Hz
+   PWM1_Start();
+   TRISA0_bit = 1;                            // PIN como entrada
+   ADC_Init_Advanced( _ADC_INTERNAL_REF );    // Voltaje de referencia a VDD y GND
 
-     while (1)
-     {
-         VALOR_ADC = ADC_Get_Sample(0) / 2.64;   //Lee el valor del ADC y lo guarda en la variable
-         PWM1_Set_Duty (VALOR_ADC);
-     }
+   while ( 1 ) {
+      VALOR_ADC = ADC_Get_Sample( 0 ) / 2.64; // Lee el valor del ADC y lo guarda en la variable
+      PWM1_Set_Duty( VALOR_ADC );
+   }
 }
